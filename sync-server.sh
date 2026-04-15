6f0c9fd9d6961cc34580b8f8692a92e41aafbbdd
niridium <niridium@tuta.io>
2026-04-16 00:36:55 +0200

    fix(sync-server.sh): enable --recursive parameter --recursive is not enabled by default when --archive is used in combination with --files-from

--- sync-server.sh
+++ sync-server.sh
@@ -1,1 +1,1 @@
-rsync -Pavhiz --files-from=/home/nixy/my_scripts/sync-files.txt /home/nixy/ $1:/storage/Home/ 
+rsync -Parvhiz --files-from=/home/nixy/my_scripts/sync-files.txt /home/nixy/ $1:/storage/Home/
