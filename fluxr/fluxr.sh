ee774b6df5a94d0678b33b68a982db9c7ac47c9f
niridium <niridium@tuta.io>
2026-05-14 09:25:38 +0200

    WIP: fluxr backup propagation

--- fluxr/fluxr.sh
+++ fluxr/fluxr.sh
@@ -4,44 +4,60 @@ cd "$(dirname "$0")" || exit
 
 source fluxr.conf
 
-# Stage 1
+# Directories shown in the logs are relative to the host
+
 core_backup() {
+    echo Host: "$hostname" "-->"
+
     if [[ -z "$REMOTE_HOST" ]]; then
-        echo -e "\tBackup directory is: $BACKUP_DIR_1/$hostname/"
-        echo -=-
+
+        echo -e "\tBacking up $ROOT --> $BACKUP_DIR/$hostname/"
+        echo ">>>"
 
         if $IS_REMOTE; then
             rsync $RSYNC_OPTS "$hostname".include "$hostname":"$ROOT"
             ssh -T "$hostname" <<EOF
                 cd $ROOT
-                rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$BACKUP_DIR_1"/"$hostname"/
+                rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$BACKUP_DIR"/"$hostname"/
 EOF
             unset IS_REMOTE
         else
-            rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$BACKUP_DIR_1"/"$hostname"/
+            rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$BACKUP_DIR"/"$hostname"/
         fi
-
     else
-        echo -e "\tBackup directory is: $REMOTE_HOST:$BACKUP_DIR_1/$hostname/"
-        echo -=-
-        rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$REMOTE_HOST":"$BACKUP_DIR_1"/"$hostname"/
+        echo -e "\tBacking up $ROOT --> $REMOTE_HOST:$BACKUP_DIR/$hostname/"
+        echo ">>>"
+        rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$REMOTE_HOST":"$BACKUP_DIR"/"$hostname"/
         unset REMOTE_HOST
     fi
 }
 
-backup_1() {
+stage_1() {
+    echo "+++ START STAGE 1"
     for hostname in "${HOSTNAMES[@]}"; do
-
         source "$hostname".conf
-
-        echo Host: "$hostname" "-->"
-        echo -e "\tDirectory to backup is: $ROOT"
 
         core_backup
     done
 }
 
-# backup_2() {
-#     rsync $RSYNC_OPTS $BACKUP_DIR_1 $BACKUP_DIR_2
-# }
-backup_1
+stage_2() {
+    echo "+++ START STAGE 2"
+
+    if [[ $(hostname) != "$BACKUP_DIR_HOST" && $(hostname) != "$PROPAGATION_1_HOST" ]]; then
+        echo -e "!!! \tCan't propagate within two remote directories"
+        exit 1
+    elif [[ $(hostname) == "$BACKUP_DIR_HOST" ]]; then
+        echo -e "??? \tBackup directory is local"
+        echo -e "??? \tPropagation directory is remote"
+        rsync $RSYNC_OPTS $BACKUP_DIR/ $PROPAGATION_1_HOST:$PROPAGATION_1/
+    else
+        echo -e "??? \tBackup directory is remote"
+        echo -e "??? \tPropagation directory is local"
+        rsync $RSYNC_OPTS $BACKUP_DIR_HOST:$BACKUP_DIR/ $PROPAGATION_1/
+    fi
+    echo "<><><> FLUX COMPLETED"
+    exit 0
+}
+stage_1
+stage_2
